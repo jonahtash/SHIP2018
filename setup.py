64f1ff752471d3dccedb7b5f528bdeff1ec04a01
jonahtash <jonahntash@gmail.com>
2018-07-27 15:53:16 -0400

    added ruby deps

--- setup.py
+++ setup.py
@@ -1,4 +1,9 @@
 from pip._internal import main
+from os import system
 
 main(['install','bs4'])
 main(['install', 'pandas'])
+system("gem install mechanize")
+system("gem install nokogiri")
+system("gem install selenium-webdriver")
+system("gem install camping")
