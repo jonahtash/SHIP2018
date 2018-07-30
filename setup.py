5c424c5c34ac1903da3fe2ee75d5e46a5faa9d5d
jonahtash <jonahntash@gmail.com>
2018-07-30 16:49:38 -0400

    added dep

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from os import system
 
 main(['install','bs4'])
 main(['install', 'pandas'])
+main(['install', 'strconv'])
 system("gem install mechanize")
 system("gem install nokogiri")
 system("gem install selenium-webdriver")
